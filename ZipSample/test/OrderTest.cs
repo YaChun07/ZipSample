6f74b1eb4ebcb77d0ce2902563408659d5a63bad
amanda.yang93 <amanda.yang@titansoft.com.sg>
2018-11-17 16:32:43 +0800

    創造一個order，完成order和orderDto的mapping

---
+++ ZipSample/test/OrderTest.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ZipSample.test
+{
+    [TestClass]
+    public class TitanTests
+    {
+        [TestMethod]
+        public void order_to_dto()
+        {
+            var order = new Order()
+            {
+                OrderId = 1,
+                Amount = 100,
+                CreateDate = new DateTime(2018, 11, 17)
+            };
+            var actual = order.Project(order1 => new OrderDto()
+            {
+                Id = order1.OrderId,
+                TotalAmount = order1.Amount,
+                Date = order1.CreateDate.ToString("yyyyMMdd")
+            });
+
+            var expected = new OrderDto()
+            {
+                Id = 1,
+                TotalAmount = 100,
+                Date = "20181117"
+            };
+            Assert.AreEqual(expected.Id, actual.Id);
+            Assert.AreEqual(expected.TotalAmount, actual.TotalAmount);
+            Assert.AreEqual(expected.Date, actual.Date);
+        }
+    }
+
+    public static class Extensions
+    {
+        public static OrderDto Project(this Order order, Func<Order, OrderDto> selector)
+        {
+            return selector(order);
+        }
+    }
+
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Date { get; set; }
+    }
+
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int Amount { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
